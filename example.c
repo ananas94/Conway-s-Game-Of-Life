ea537bc6dbc65caaf2680e78f6a624ee7acb77e4
yaAnanas <muden.h@gmail.com>
2014-01-08 02:55:16 +0600

    i want to sleep and this doesn't work but i want to sleep  so much

--- example.c
+++ example.c
@@ -6,26 +6,34 @@ static char *backgroundTexture;
 static char *lifeTexture;
 static char *lonelinessTexture;
 static char *overcrowdingTexture;
-static 
+static short field[35][32];
 void reshape(int w, int h)
 {
-  //  printf("%d %d \n",w, h );  here's some bag... sometimes w!=700 or h!=700
+  //  printf("%d %d \n",w, h );  here's some bag... sometimes w!=699 or h!=699
   // but glutReshpeWindow don't change window size
-    if ((w!=700) || (h!=700)){
-          glutReshapeWindow(700,700);
+    if ((w!=699) || (h!=699)){
+          glutReshapeWindow(699,699);
         }
-                    glViewport(0, 0, w, h);
+                    glViewport(0, 0, 700, 700);
            
-            glMatrixMode(GL_PROJECTION);
-            glLoadIdentity();
-            gluOrtho2D(0, w, 0, h);
-
-            glMatrixMode(GL_MODELVIEW);
-            glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(0, 700, 0, 700);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
-void mouseclick( int button, int state, int x, int y )
+void mouseClick( int button, int state, int x, int y )
 {
-    printf("%d %d \n",x,y); 
+    printf("%d %d \n",x/20,y/20); 
+    if (button==GLUT_LEFT_BUTTON)
+    {
+      field[x/20][y/20]=ALIVE;
+    }
+}
+void mouseMotion(int x, int y)
+{
+    field[x/20][y/20]=ALIVE;
+    printf("%d %d \n",x/20,y/20); 
 }
 char* readTexture(char* fileName, int alpha)
 {
@@ -55,41 +63,56 @@ void display()
 {
     int i,j;
     glClear(GL_COLOR_BUFFER_BIT);
+    //background
     if(backgroundTexture==NULL)
     {
         glBegin(GL_QUADS);
             glColor3f(0.0, 1.0, 1.0);
               glVertex2i(0, 0);
-              glVertex2i(700,0);
-              glVertex2i(700,700);
-              glVertex2i(0,700);
+              glVertex2i(699,0);
+              glVertex2i(699,699);
+              glVertex2i(0,699);
         glEnd();
     }
     else
     {
-        glDrawPixels(700, 700, GL_BGR, GL_UNSIGNED_BYTE, backgroundTexture);
+        glDrawPixels(699, 699, GL_BGR, GL_UNSIGNED_BYTE, backgroundTexture);
     }
-
+    //lines
     for (i=0;i<70;i++)
       {
         glBegin(GL_LINES);
           glColor3f(0.0, 0.0, 0.0);
           glVertex2i(0,i*20);
-          glVertex2i(700,i*20);
-        glEnd();
-      }
-      for (i=0;i<70;i++)
-      {
-        glBegin(GL_LINES);
-          glColor3f(0.0, 0.0, 0.0);
+          glVertex2i(699,i*20);
           glVertex2i(i*20,0);
-          glVertex2i(i*20,700);
+          glVertex2i(i*20,699);
+
         glEnd();
       }
+      for (i=0;i<35;i++)
+        for (j=0;j<32;j++)
+          if (field[i][j]==ALIVE)
+          {
+            glBegin(GL_QUADS);
+              glColor3f(1.0, 1.0, 1.0);
+              glVertex2i(i*20, j*20);
+              glVertex2i((i+1)*20,j*20);
+              glVertex2i((i+1)*20,(j+1)*20);
+              glVertex2i(i*20,(j+1)*20);
+            glEnd();
+
+          }
+
 
     glutSwapBuffers();
 }
-
+void step(int value){
+  static int i=0;
+ //here must be rules
+  display();
+  glutTimerFunc(1000,step,1);
+}
 int main (int argc, char * argv[])
 {
         backgroundTexture=readTexture("backgroundTexture.bmp",0);
@@ -97,16 +120,16 @@ int main (int argc, char * argv[])
         lifeTexture=readTexture("life.bmp",1);
         lonelinessTexture=readTexture("loneliness.bmp",1);
 
-
         glutInit(&argc, argv);
         glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
-        glutInitWindowSize(700, 700);
+        glutInitWindowSize(699, 699);
         glutCreateWindow("Lab5");       
         glutReshapeFunc(reshape);
         glutDisplayFunc(display);
-        glutMouseFunc(mouseclick);
+        glutMouseFunc(mouseClick);
+        glutMotionFunc(mouseMotion);
+        glutTimerFunc(1000,step,1);
         glutMainLoop();
         free(backgroundTexture);
         return 0;
 }
-
